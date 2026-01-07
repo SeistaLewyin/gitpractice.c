6979eba826592889a707aa9859e5f618b2fe9264
SeistaLewyin <seistalewyin@gmail.com>
2026-01-07 12:35:54 +0600

    Last correction

--- grade-sheet1.c
+++ grade-sheet1.c
@@ -8,23 +8,42 @@ int main()
     for(i=0; i<num; i++)
     {
         printf("For student no. %d\n", i+1);
+        TT:
         printf("Enter your marks in Term Test: ");
         scanf("%d", &mark_ind[i][0]);
+        if(mark_ind[i][0]<0 || mark_ind[i][0]>20)
+        {
+            printf("Your input is inaccurate\n");
+            goto TT;
+        }
+        AT:
         printf("Enter your marks in Attendance: ");
         scanf("%d", &mark_ind[i][1]);
+        if(mark_ind[i][1]<0 || mark_ind[i][1]>10)
+        {
+            printf("Your input is inaccurate\n");
+            goto AT;
+        }
+        AS:
         printf("Enter your marks in Assignment: ");
         scanf("%d", &mark_ind[i][2]);
+        if(mark_ind[i][2]<0 || mark_ind[i][2]>10)
+        {
+            printf("Your input is inaccurate\n");
+            goto AS;
+        }
+        FE:
         printf("Enter your marks in Final: ");
         scanf("%d", &mark_ind[i][3]);
-        if(mark_ind[i][0]<=20 && mark_ind[i][1]<=10 && mark_ind[i][2]<=10 && mark_ind[i][3]<=60)
+        if(mark_ind[i][3]<0 || mark_ind[i][3]>60)
         {
-            mark[i] = mark_ind[i][0]+mark_ind[i][1]+mark_ind[i][2]+mark_ind[i][3];
-        }else{
             printf("Your input is inaccurate\n");
+            goto FE;
         }
+        mark[i] = mark_ind[i][0]+mark_ind[i][1]+mark_ind[i][2]+mark_ind[i][3];
     }
     printf("----------------------------------------------------------\n");
-    printf("Serial no.    TT    AT    CE    FE    Total Marks    Grade\n");
+    printf("Serial no.    TT    AT    AS    FE    Total Marks    Grade\n");
     printf("----------------------------------------------------------\n");
     for(i=0; i<num; i++)
     {
@@ -40,5 +59,5 @@ int main()
         else printf("    %d.        %d    %d     %d     %d         %d         F", i+1,mark_ind[i][0],mark_ind[i][1],mark_ind[i][2],mark_ind[i][3],mark[i]);
         printf("\n");
     }
-    printf("---------------------------------------------------------\n");
+    printf("----------------------------------------------------------\n");
 }
